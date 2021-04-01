ffc5539d6cdf3fa739bddef83ebf74cbe5878a1c
aonemd <aonemdsaleh@gmail.com>
2021-04-01 18:49:42 +0200

    Assign order to each parsed bar

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,22 @@ struct Bars {
     bar: Vec<Bar>,
 }
 
+impl Bars {
+    fn assign_orders(&mut self) {
+        for (index, bar) in self.bar.iter_mut().enumerate() {
+            bar.order = (index + 1) as u32;
+        }
+    }
+}
+
 #[derive(Debug, Deserialize)]
 struct Bar {
     name: String,
     command: String,
     args: Option<Vec<String>>,
     interval: i32,
+    #[serde(skip)]
+    order: u32,
 }
 
 impl Bar {
@@ -29,7 +39,9 @@ impl Bar {
 
 fn main() -> Result<(), std::io::Error> {
     let bars_as_toml = std::fs::read_to_string("bars.toml")?;
-    let bars: Bars = toml::from_str(&bars_as_toml).unwrap();
+    let mut bars: Bars = toml::from_str(&bars_as_toml).unwrap();
+
+    bars.assign_orders();
 
     let status: Vec<String> = bars.bar.into_iter().map(|b| b.run()).rev().collect();
 
