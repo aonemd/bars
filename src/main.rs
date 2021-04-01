660014512c68994bbbadb2c08a8bcba9a3c13172
aonemd <aonemdsaleh@gmail.com>
2021-04-01 16:33:58 +0200

    Read and parse `bars.toml` into Bar structs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,30 @@
 use std::process::Command;
 
-fn main() {
-    let out = Command::new("./bars/date_time_component.sh").output().expect("Error occurred");
+use serde::Deserialize;
+use toml;
+
+#[derive(Debug, Deserialize)]
+struct Bars {
+    bar: Vec<Bar>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Bar {
+    name: String,
+    command: String,
+    interval: i32,
+}
+
+fn main() -> Result<(), std::io::Error> {
+    let bars_as_toml = std::fs::read_to_string("bars.toml")?;
+    let bars: Bars = toml::from_str(&bars_as_toml).unwrap();
+
+    let out = Command::new("./bars/date_time_component.sh")
+        .output()
+        .expect("Error occurred");
     let time = String::from_utf8_lossy(&out.stdout);
     let status = format!("Time: {}", time);
     let _ = Command::new("xsetroot").arg("-name").arg(status).output();
+
+    Ok(())
 }
