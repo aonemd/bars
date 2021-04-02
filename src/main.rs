c136dea229a2482692c4e4bdcabb53f563074e7e
aonemd <aonemdsaleh@gmail.com>
2021-04-02 07:39:19 +0200

    WIP: Run each bar in its own thread

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,72 @@
 use std::process::Command;
-use std::sync::mpsc::{self, Sender, Receiver};
+use std::time::Duration;
+use std::thread;
+use std::sync::mpsc::{sync_channel, SyncSender, Receiver};
 
 use serde::Deserialize;
 use toml;
 
 #[derive(Debug)]
-struct BarChannel<T>(Sender<T>, Receiver<T>);
-impl<T> Default for BarChannel<T> {
+struct BarChannel<String>(SyncSender<String>, Receiver<String>);
+impl Default for BarChannel<String> {
     fn default() -> Self {
-        let (sender, receiver) = mpsc::channel();
+        let (sender, receiver) = sync_channel::<String>(0);
         Self(sender, receiver)
     }
 }
 
 #[derive(Debug, Deserialize)]
-struct Bars<T> {
-    bar: Vec<Bar<T>>,
+struct Bars {
+    bar: Vec<Bar>,
 
     #[serde(skip)]
-    channel: BarChannel<T>,
+    channel: BarChannel<String>,
 }
 
-impl<T> Bars<T> {
+impl Bars {
     pub fn assign_default_attributes(&mut self) {
         for (index, bar) in self.bar.iter_mut().enumerate() {
             bar.order = (index + 1) as u32;
             bar.sender = self.channel.0.clone();
         }
     }
+
+    pub fn run(self) {
+        for bar in self.bar.into_iter() {
+            thread::spawn(move || {
+                loop {
+                    let res = bar.run();
+                    bar.sender.send(res).unwrap();
+                    thread::sleep(Duration::from_secs(bar.interval));
+                }
+            });
+        }
+
+        for received in &self.channel.1 {
+            println!("Got: {}", received);
+        }
+
+    }
 }
 
 #[derive(Debug, Deserialize)]
-struct Bar<T> {
+struct Bar {
     name: String,
     command: String,
     args: Option<Vec<String>>,
-    interval: i32,
+    interval: u64,
     #[serde(skip, default = "default_channel_sender")]
-    sender: Sender<T>,
+    sender: SyncSender<String>,
     #[serde(skip)]
     order: u32,
 }
 
-fn default_channel_sender<T>() -> Sender<T> {
-    let (_sender, _) = mpsc::channel();
+fn default_channel_sender() -> SyncSender<String> {
+    let (_sender, _) = sync_channel::<String>(0);
     _sender
 }
 
-impl<T> Bar<T> {
+impl Bar {
     fn run(&self) -> String {
         let out = Command::new(&self.command)
             .args(self.args.as_ref().unwrap_or(&vec![]))
@@ -60,14 +79,17 @@ impl<T> Bar<T> {
 
 fn main() -> Result<(), std::io::Error> {
     let bars_as_toml = std::fs::read_to_string("bars.toml")?;
-    let mut bars: Bars<String> = toml::from_str(&bars_as_toml).unwrap();
+    let  mut bars: Bars = toml::from_str(&bars_as_toml).unwrap();
 
     bars.assign_default_attributes();
+    bars.run();
 
-    let status: Vec<String> = bars.bar.into_iter().map(|b| b.run()).rev().collect();
+    // println!("{:#?}", bars);
 
-    let status = status.join(" | ");
-    let _ = Command::new("xsetroot").arg("-name").arg(status).output();
+    // let status: Vec<String> = bars.bar.into_iter().map(|b| b.run()).rev().collect();
+
+    // let status = status.join(" | ");
+    // let _ = Command::new("xsetroot").arg("-name").arg(status).output();
 
     Ok(())
 }
