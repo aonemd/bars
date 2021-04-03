a7d029ad96b007f56d5ff9bfc04ba05abf8a5b5b
aonemd <aonemdsaleh@gmail.com>
2021-04-03 16:19:08 +0200

    Add volume bar

---
+++ bars/volume.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+SOUND_CARD="0"
+# find an entry that has `headphone jack` in it using `amixer controls`
+HEADPHONE_NUMID="18"
+
+main() {
+  print_volume
+
+  exit 0
+}
+
+print_volume() {
+  local speaker_up_icon=""
+  local speaker_mute_icon=" MUTE"
+  local headphone_up_icon=""
+  local headphone_mute_icon=" MUTE"
+
+  parse_volume
+  case "${state}" in
+    on)
+      current_icon=$speaker_up_icon
+      if [ "$headphone_state" == "on" ]; then
+        current_icon=$headphone_up_icon
+      fi
+
+      volume="${current_icon} ${level}%"
+      ;;
+    off)
+      current_icon=$speaker_mute_icon
+      if [ "$headphone_state" == "on" ]; then
+        current_icon=$headphone_mute_icon
+      fi
+
+      volume="${current_icon}"
+      ;;
+  esac
+  echo "${volume}"
+}
+
+parse_volume() {
+  local response=$(amixer get Master)
+  level=$(echo "$response" | grep -o "\[[0-9]\+%\]" | sed "s/[^0-9]*//g")
+  state=$(echo "$response" | grep -o "\[\(on\|off\)\]" | sed "s/[^(on|off)]//g")
+
+  local headphone_response=$(amixer -c "$SOUND_CARD" cget numid="$HEADPHONE_NUMID")
+  headphone_state=$(echo "$headphone_response" | grep -o "values=\(on\|off\)" | sed "s/[^(on|off)]//g")
+
+}
+
+main "$@"
