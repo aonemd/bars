d643b448adc894b7d869e762e147f06e3c986835
aonemd <aonemdsaleh@gmail.com>
2021-04-03 18:51:24 +0200

    Add colors to volume bar and add nocolor version

--- bars/volume_nocolor.sh
+++ bars/volume_nocolor.sh
@@ -12,9 +12,9 @@ main() {
 
 print_volume() {
   local speaker_up_icon=""
-  local speaker_mute_icon="^c#f22c40^ MUTE^c#a8a19f^"
+  local speaker_mute_icon=" MUTE"
   local headphone_up_icon=""
-  local headphone_mute_icon="^c#f22c40^ MUTE^c#a8a19f^"
+  local headphone_mute_icon=" MUTE"
 
   parse_volume
   case "${state}" in
