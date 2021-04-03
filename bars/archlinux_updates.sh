22427c49babbe1c447610a5d89e3fc035fd2f211
aonemd <aonemdsaleh@gmail.com>
2021-04-03 19:27:30 +0200

    Add Arch Linux updates bar with colors

---
+++ bars/archlinux_updates.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# requires `pacman-contrib`
+#
+
+LOWER_UPDATE_LIMIT=50
+HIGHER_UPDATE_LIMIT=100
+
+main() {
+  update_counter=$(checkupdates | wc -l)
+  [[ $update_counter -le $LOWER_UPDATE_LIMIT ]] && exit 0
+
+  if [ $update_counter -ge $HIGHER_UPDATE_LIMIT ]; then
+    echo "^c#f22c40^$update_counter^c#a8a19f^"
+  else
+    echo "$update_counter"
+  fi
+
+  exit 0
+}
+
+main "$@"
