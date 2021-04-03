934aa89a055e01ccaa60d2354a94269de00a1e9a
aonemd <aonemdsaleh@gmail.com>
2021-04-03 18:36:25 +0200

    Read config_file from command line options

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 use std::collections::HashMap;
 
+use structopt::StructOpt;
+
 mod bar;
 use bar::Runner;
 
+#[derive(Debug, StructOpt)]
+struct Cli {
+    #[structopt(short = "C", long = "config", help = "Config file to load", parse(from_os_str), default_value = "bars.toml")]
+    config_file: std::path::PathBuf,
+}
+
 fn main() -> Result<(), std::io::Error> {
-    let config = std::fs::read_to_string("bars.toml")?;
+    let cli = Cli::from_args();
+    let config = std::fs::read_to_string(cli.config_file)?;
 
     let bar_runner = Runner::from(config);
 
